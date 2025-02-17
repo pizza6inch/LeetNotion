3884482563c16ace607bd985e746eccfefaf2ae4
huiwu Zhang <pizza6inch@gmail.com>
2025-02-17 16:37:16 +0800

    test api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import axios from "axios";
+
+const fetchDailyProblem = async () => {
+  try {
+    const response = await axios.get("https://alfa-leetcode-api.onrender.com/daily");
+    console.log(response.data);
+  } catch (error) {
+    console.error("error fetching leetcode problem:", error);
+  }
+};
+
+// fetchDailyProblem();
